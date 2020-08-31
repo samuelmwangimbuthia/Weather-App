0227d1e5520cc1226f44e92b40a719cc318015f3
Samuel <mbuthia.jsamuel@gmail.com>
2020-08-31 07:25:27 +0300

    feature(dev-server):add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,27 @@ module.exports = {
     mode: 'development',
     entry: './src/main.js',
     output: {
-        path:__dirname + '/src',
+        path:__dirname + './dist',
         filename: 'bundle.js'
-    }
+    },
+    
+    module: {
+        rules: [
+            {
+                test:/\.js&/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: './dist',
+    },
+
 }
